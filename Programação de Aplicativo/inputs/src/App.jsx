ecc9d2c3afb1a9c4dab25a9189aabcb386f72ce4
CAMILA LISTON <camila_liston@estudante.sesisenai.org.br>
2025-04-03 21:07:33 -0300

    atualização aula 03/04/2024

---
+++ Programação de Aplicativo/inputs/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const[inputUsername, setInputUsername] = useState('')
+  const[inputSenha, setInputSenha] = useState('')
+
+  function efetuarLogin(){
+    alert(inputUsername)
+  }
+
+  return (
+    <div className='container-app'>
+      <h2>Inputs Controlados</h2>
+      <label htmlFor="">Username</label>
+      <input type="text" 
+        value={inputUsername}
+        onChange={(event) => setInputUsername(event.target.value)}
+      />
+
+      <label htmlFor="">Senha</label>
+      <input type="text" 
+      value={inputSenha}
+      onChange={(value) => setInputSenha (event.target.value)}
+      />
+
+      
+      {/* {inputValor} */}
+
+      <button onClick={efetuarLogin}>Login</button>
+
+    </div>
+  )
+}
+
+export default App
