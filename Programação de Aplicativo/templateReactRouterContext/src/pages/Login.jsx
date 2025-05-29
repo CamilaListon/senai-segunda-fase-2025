98f3b3013d77cf8144ea71d979aaf5178495dafd
CamilaListon <c.liston@hotmail.com>
2025-05-28 21:42:19 -0300

    Aula de login cm router

--- Programação de Aplicativo/templateReactRouterContext/src/pages/Login.jsx
+++ Programação de Aplicativo/templateReactRouterContext/src/pages/Login.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react'
 import { useState } from "react"
 import { GlobalContext } from '../contexts/GlobalContext'
+import { useNavigate } from 'react-router-dom'
 
 
 function Login() {
     const [inputUsername, setInputUsername] = useState('')
     const [inputSenha, setInputSenha] = useState('')
     const{usuarios} = useContext(GlobalContext)
+    const navigate  = useNavigate()
 
     function efetuarLogin(){
         const usuario = usuarios.find((u) => u.username == inputUsername && u.senha == inputSenha)
         if(usuario){
             alert("Login efetuado")
+            navigate('/home')
+        }
+        else{
+            alert("Credenciais incorretas")
         }
     }
     return (        
